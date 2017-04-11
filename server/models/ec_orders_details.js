18589e42f3f668680db3ea3e5ffb163756d027b6
佑佑科技 <apple2@ioioinfo.com>
2017-04-11 09:49:21 +0800

    更新

--- server/models/ec_orders_details.js
+++ server/models/ec_orders_details.js
@@ -5,7 +5,7 @@ var ec_orders_details = function(server) {
 	return {
 		//获取所有订单详细
 		search_ec_order_details: function(order_ids,cb){
-			var query = `select order_id,product_id,number,price,order_index,marketing_price,total_price
+			var query = `select order_id,product_id,number,price,order_index,marketing_price,total_price,sku_id
 				from ec_orders_details where order_id in (?) and flag =0`;
 			server.plugins['mysql'].pool.getConnection(function(err, connection) {
 				connection.query(query, [order_ids], function(err, results) {
